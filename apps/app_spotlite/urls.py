0f3288281adec414094a4fb20e338b9e9319c730
Kadek Sudiartha <sudieartha@gmail.com>
2018-06-25 17:34:06 +0800

    audio player first setup

--- apps/app_spotlite/urls.py
+++ apps/app_spotlite/urls.py
@@ -5,7 +5,8 @@ app_name = 'app_spotlite'
 
 urlpatterns = [
     path('profile', views.profile, name='profile'),
-    path('settings', views.settings, name='settings'), 
+    path('settings', views.settings, name='settings'),
+    path('dashboard', views.dashboard, name='dashboard'),
 
     path('', views.index, name='index'),
 ]
--- apps/app_spotlite/views.py
+++ apps/app_spotlite/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'app_spotlite/index.html')
 
+def dashboard(request):
+    return render(request, 'app_spotlite/dashboard.html')
+
 def profile(request):
     pass
 
