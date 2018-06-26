26c0be7aa231e12354df4f5789b7172a987f2c1e
Glenn Note <glenn.note@gmail.com>
2018-06-26 18:05:31 +0800

    deleted database

--- apps/auth_spotlite/models.py
+++ apps/auth_spotlite/models.py
@@ -8,6 +8,7 @@ class User(models.Model):
     surname = models.CharField(max_length=32)
     password = models.CharField(max_length=128)
     profilepic = models.CharField(max_length=128)
+    premium = models.IntegerField(default=0)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
