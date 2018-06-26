ec6fb8910a2f3332d7ccfce4cd770f7144b3617a
nawikart <nawi@outpost-asia.com>
2018-06-26 12:16:45 +0800

    collaborate playlist

--- apps/app_spotlite/urls.py
+++ apps/app_spotlite/urls.py
@@ -6,8 +6,17 @@ app_name = 'app_spotlite'
 urlpatterns = [
     path('', views.index, name='index'),
 
+    path('playlists-editor', views.playlists_editor, name='playlists_editor'),
     path('playlists', views.my_playlists, name='my_playlists'),
+    path('playlist/<int:playlist_id>/like', views.like_playlist, name='like_playlist'),
+    
     path('playlist/<int:playlist_id>/delete', views.delete_playlist, name='delete_playlist'),
     path('playlist/<int:playlist_id>/edit', views.edit_playlist, name='edit_playlist'),
-    path('<int:user_id>/playlists', views.user_playlists, name='user_playlists'),    
+    path('<int:user_id>/playlists', views.user_playlists, name='user_playlists'),
+
+    path('playlist/<int:playlist_id>/items', views.items_in_playlist, name='items_in_playlist'),
+    path('playlist/items/<int:item_id>/delete', views.delete_item_in_playlist, name='delete_item_in_playlist'),
+
+    path('song/<int:song_id>/add-to-playlist', views.add_to_playlist_step1, name='add_to_playlist_step1'),
+    path('song/<int:song_id>/add-to-playlist/<int:playlist_id>', views.add_to_playlist_step2, name='add_to_playlist_step2'),    
 ]
--- apps/app_spotlite/views.py
+++ apps/app_spotlite/views.py
@@ -9,52 +9,118 @@ from pprint import pprint
 def index(request):
     return render(request, 'app_spotlite/index.html')
 
+def add_to_playlist_step1(request, song_id):
+    if 'user_id' in request.session:
+        context = {
+            'editors': m.Editor.objects.filter(user_id=request.session['user_id']),
+            'song_id': song_id
+        }
+        return render(request, 'app_spotlite/playlists.html', context)
 
-def my_playlists(request):
+    return redirect('app_spotlite:index')
+
+
+def add_to_playlist_step2(request, song_id, playlist_id):
+    if 'user_id' in request.session:
+        if len(m.PlaylistItem.objects.filter(user_id=request.session['user_id'], song_id=song_id, playlist_id=playlist_id)) == 0:
+            playlist_item = m.PlaylistItem()
+            playlist_item.song_id = song_id
+            playlist_item.playlist_id = playlist_id
+            playlist_item.user_id = request.session['user_id']
+            playlist_item.save()
+        
+        return redirect('app_spotlite:my_playlists')
+
+    return redirect('app_spotlite:index')
+
+
+def playlists_editor(request):
     if 'user_id' in request.session:
         if request.method == 'POST':
             try:
                 playlist = m.Playlist()
-                playlist.content = request.POST['html_content']
-                playlist.user_id = request.session['user_id']
+                playlist.title = request.POST['html_title']
                 playlist.save()
+
+                #save to editor
+                editor = m.Editor()
+                editor.playlist_id = playlist.id
+                editor.user_id = request.session['user_id']
+                editor.save()
+
                 return redirect(request.META['HTTP_REFERER'])
             except:
                 messages.error(request, 'invalid input')
 
-        is_friends = []
-        for playlist in playlists_by_user(request, request.session['user_id']):
-            is_friends.append({'playlist_id': playlist.id, 'is_friend': is_friend(request, playlist.user_id) })
-        
         context = {
-            'playlists': playlists_by_user(request, request.session['user_id']),
-            'is_friends': is_friends
+            'editors': m.Editor.objects.filter(user_id=request.session['user_id']),
+            'can_add': True,
+            'user': False
         }
-        return render(request, 'myface/playlists.html', context = context)
+        return render(request, 'app_spotlite/playlists.html', context = context)
 
     return redirect('auth_spotlite:index')
 
 
+def my_playlists(request):
+
+    if 'user_id' in request.session:
+        context = {
+            'editors': m.Editor.objects.filter(user_id=request.session['user_id']),
+            'user': False
+        }
+        return render(request, 'app_spotlite/playlists.html', context = context)
+
+    return redirect('auth_spotlite:index')
+
+
+def items_in_playlist(request, playlist_id):
+    if 'user_id' in request.session:
+
+        context = {
+            'items': m.PlaylistItem.objects.filter(playlist_id=playlist_id),
+            'playlist': m.Playlist.objects.get(id=playlist_id),
+            'editors': m.Editor.objects.filter(playlist_id=playlist_id)
+        }
+        return render(request, 'app_spotlite/playlist-items.html', context = context)
+
+    return redirect('app_spotlite:index')
+
+
+def delete_item_in_playlist(request, item_id):
+    item = m.PlaylistItem.objects.get(id=item_id)
+    item.delete()
+    return redirect(request.META['HTTP_REFERER'])
+
 def user_playlists(request, user_id):
-    is_friends = []
-    for playlist in playlists_by_user(request, user_id):
-        is_friends.append({'playlist_id': playlist.id, 'is_friend': is_friend(request, playlist.user_id) })
-                
+
     context = {
-        'playlists': playlists_by_user(request, user_id),
+        'editors': m.Editor.objects.filter(user_id=user_id),
         'user': mu.User.objects.get(id=user_id),
-        'is_friends': is_friends
     }    
-    return render(request, 'myface/playlists.html', context = context)
+    return render(request, 'app_spotlite/playlists.html', context = context)
 
 
-def playlists_by_user(request, user_id):
-    return m.Playlist.objects.filter(user_id=user_id).order_by('-created_at')
+# def playlists_by_user(request, user_id):
+#     return m.Playlist.objects.filter(user_id=user_id).order_by('-created_at')
 
+def like_playlist(request, playlist_id):
+    if len(m.Editor.objects.filter(user_id=request.session['user_id'], playlist_id=playlist_id)) == 0:
+        editor = m.Editor()
+        editor.playlist_id = playlist_id
+        editor.user_id = request.session['user_id']
+        editor.save()
+    
+    return redirect(request.META['HTTP_REFERER'])
 
 def delete_playlist(request, playlist_id):
-    playlist = m.Playlist.objects.get(id=playlist_id)
-    playlist.delete()
+    if len(m.Editor.objects.filter(playlist_id=playlist_id)) > 1:
+        editor = m.Editor.objects.get(user_id=request.session['user_id'], playlist_id=playlist_id)
+        editor.delete()
+    else:
+        playlist = m.Playlist.objects.get(id=playlist_id)
+        playlist.delete()
+
     return redirect(request.META['HTTP_REFERER'])
 
 
@@ -62,13 +128,23 @@ def edit_playlist(request, playlist_id):
     playlist = m.Playlist.objects.get(id=playlist_id)
 
     if request.method == 'POST':
-        playlist.content = request.POST['html_content']
+        playlist.title = request.POST['html_title']
         playlist.save()
-        return redirect(request.META['HTTP_REFERER'])
+
+        if request.POST['destination'] != '':
+            return redirect(request.POST['destination'])
+        else:
+            return redirect('app_spotlite:my_playlists')
+
+    if 'HTTP_REFERER' in request.META:
+        referer = request.META['HTTP_REFERER']
+    else:
+        referer = ''
 
     context = {
-        'playlist': playlist
+        'playlist': playlist,
+        'destination': referer
     } 
 
-    return render(request, 'myface/edit.html', context = context)
+    return render(request, 'app_spotlite/playlist-edit.html', context = context)
     
