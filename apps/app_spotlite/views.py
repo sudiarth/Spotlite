e8a284c4f1c7b29e94e0605f16a341afdb8c8c50
Glenn Note <glenn.note@gmail.com>
2018-06-26 12:30:36 +0800

    fixed user model import

--- apps/app_spotlite/views.py
+++ apps/app_spotlite/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
-from apps.auth_spotlite import models as m
+from apps.auth_spotlite import models as um
 import random
 
 def index(request):
@@ -31,17 +31,16 @@ def picture_upload(request):
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
 
-        user = m.User.objects.get(id=request.session['user_id'])
+        user = um.User.objects.get(id=request.session['user_id'])
         user.profilepic = uploaded_file_url
         user.save()
 
         request.session['profilepic'] = user.profilepic
-        return render(request, 'app_spotlite/profile.html')
-
+        return redirect('app_spotlite:profile')
     return render(request, 'app_spotlite/profile.html')
 
 def update_settings(request):
-    user = m.User.objects.get(id=request.session['user_id'])
+    user = um.User.objects.get(id=request.session['user_id'])
     user.email = request.POST['html_email']
     user.firstname = request.POST['html_firstname']
     user.surname = request.POST['html_surname']
@@ -53,7 +52,7 @@ def update_settings(request):
     return redirect('app_spotlite:settings')
     
 def update_password(request):
-    user = m.User.objects.get(id=request.session['user_id'])
+    user = um.User.objects.get(id=request.session['user_id'])
     if request.POST['html_password'] == request.POST['html_confirm'] and len(request.POST['html_password']) > 0:
         user.password = request.POST['html_password']
         user.save()
